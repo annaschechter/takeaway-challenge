88cfee7baa6c22943f0db68e0247f025048eeba5
Anna Jerofejeva <jerofejeva.anna@yahoo.com>
2014-09-27 16:21:15 +0100

    Customer can now select and cancel dishes, and place order

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,10 +1,26 @@
+require_relative 'order'
+
 class Customer
 
 	attr_reader :name, :phone
-	
+
 	def initialize(name, phone)
 		@name = name
 		@phone = phone
 	end
+
+	def select_dish(menu, dish, qty)
+		@order = Order.new
+		@order.add_item(menu, dish, qty)
+	end
+
+	def remove_dish(dish)
+	    @order.cancel_item(dish)
+	end
+
+	def place_order
+		raise "There are no orders to place" if @order == nil
+	    @order.complete_order!
+	end 
 
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -3,6 +3,7 @@ require 'customer'
 describe Customer do 
 
  let(:customer) {Customer.new("Anna Jerofejeva", 07746010777)}
+ let(:order) {double :order}
 
 	it "should initialize with name" do
        expect(customer.name.class).to eq(String)
@@ -11,5 +12,10 @@ describe Customer do
     it "should initialize with telephone number" do
        expect(customer.phone.class).to eq(Fixnum)
     end
+
+    it "should only be able to place orders that are not empty" do
+    	expect(lambda {customer.place_order}).to raise_error "There are no orders to place"
+    end
+
 	
 end
