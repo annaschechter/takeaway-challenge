d205344e493067c259d2cfd8ad9d6736f689b4b2
Anna Jerofejeva <jerofejeva.anna@yahoo.com>
2014-09-28 11:37:44 +0100

    Text includes delivery time one hour from placing the order

--- lib/message.rb
+++ lib/message.rb
@@ -3,19 +3,26 @@ require 'twilio-ruby'
 
 class Message
 
+	attr_reader :text
+
 	def initialize
 		@sent = false
 	end
 
+	def create_text
+		@delivery_time = Time.now + 60 * 60
+		@text = "Thank you! Your order was placed and will be delivered before #{@delivery_time.strftime('%H:%M')}."
+    end
+
 	def send_text
-		@message = "Thank you! Your order was placed."
+		create_text
 		account_sid = "AC91a6dee08bbe2d14e7a6b175e6dfb6a8"
         auth_token = "0c10b5dfbc3fc1f98125a847db54d0d3"
         @client = Twilio::REST::Client.new account_sid, auth_token
         @client.account.messages.create({
 		:from => '+441305300063', 
 		:to => '+447746010888', 
-		:body => "#{@message}"
+		:body => "#{@text}"
 		})
 
 		@sent = true
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -8,9 +8,4 @@ describe Message do
 		expect(message).to_not be_sent
 	end
 
-	it "should be able to update to sent" do
-		message.send_text
-		expect(message).to be_sent
-	end
-
 end
